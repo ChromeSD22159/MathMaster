3a2866f01f75224747811485c16f5d489279e107
Leonid Riedel <leonid.riedel@gmail.com>
2024-12-02 11:50:53 +0100

    init project

---
+++ MathMaster/MathMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathMaster
+//
+//  Created by Leonid Riedel on 02.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathMaster/MathMaster/MathMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasterApp.swift
+//  MathMaster
+//
+//  Created by Leonid Riedel on 02.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MathMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
