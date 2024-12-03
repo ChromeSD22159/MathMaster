03e617a1e6244cd6d8bb8201e2eea7a760baf172
RobertLaas <laas.robert@gmail.com>
2024-12-03 14:36:41 +0100

    Ticket 1.6 vorerst fertig

--- MathMaster/MathMaster/Views/SetupView.swift
+++ MathMaster/MathMaster/Views/SetupView.swift
@@ -8,46 +8,58 @@
 import SwiftUI
 
 struct SetupView: View {
-    //TODO: refektor
+    //TODO: refektor in englisch
     @AppStorage("Schwierigkeitsgrad") private var selectedPicker = Schwierigkeitsgrad.easy
     @AppStorage("gameDuration") private var gameDuration = 60
-    @State private var showSheet = false
+//    @State private var showSheet = false
+    @AppStorage("additionToggle") private var additionToggle = false
+    @AppStorage("subtractionToggle") private var subtractionToggle = false
+    @AppStorage("multiplicationToggle") private var multiplicationToggle = false
+    @AppStorage("divisionToggle") private var divisionToggle = false
     
     var body: some View {
         
-        VStack {
-            Text("Schwierigkeitsgrad wählen")
-                .font(.headline)
-                .bold()
-            Picker("Schwierigkeitsgrad", selection: $selectedPicker) {
-                ForEach(Schwierigkeitsgrad.allCases) { gameType in
-                    Text(gameType.rawValue).tag(gameType)
+        NavigationStack {
+            VStack {
+                Text("Schwierigkeitsgrad wählen")
+                    .font(.headline)
+                    .bold()
+                Picker("Schwierigkeitsgrad", selection: $selectedPicker) {
+                    ForEach(Schwierigkeitsgrad.allCases) { gameType in
+                        Text(gameType.rawValue).tag(gameType)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding()
+                
+                Form {
+                    Section("Rundenlänge") {
+                        Stepper("Rundenlänge: \(gameDuration) sek.", value: $gameDuration, in: (30...90), step: 10)
+                        
+                        NavigationLink("Statistik"){
+                            StatsView()
+//                            Button {
+//                                showSheet = true
+//                            } label: {
+//                                Text("Statistik")
+//                            }
+//                         .buttonStyle(.borderedProminent)
+                        }
+//                        .sheet(isPresented: $showSheet) {
+//                            StatsView()
+//                                .presentationDetents([.large,.medium])
+//                        }
+                    }
+                    
+                    Section("Grundrechenarten") {
+                        Toggle("Addition", systemImage: "plus", isOn: $additionToggle)
+                        Toggle("Subtraktion", systemImage: "minus", isOn: $subtractionToggle)
+                        Toggle("Multiplikation", systemImage: "multiply", isOn: $multiplicationToggle)
+                        Toggle("Division", systemImage: "divide", isOn: $divisionToggle)
+                    }
                 }
             }
-            .pickerStyle(.segmented)
-            
-            Stepper("Rundenlänge: \(gameDuration) sek.", value: $gameDuration, in: (30...90), step: 10)
-            
-            Spacer()
-            
-            Button {
-                showSheet = true
-            } label: {
-                Text("Statistik")
-            }
-            .buttonStyle(.borderedProminent)
-            .sheet(isPresented: $showSheet) {
-                StatsView()
-                    .presentationDetents([.large,.medium])
-            }
-            
-            
         }
-        .padding()
-        
-        
-        
-        
     }
 }
 
