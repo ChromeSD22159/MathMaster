81db1042404b1926d3cb9f17b73e808091de7647
RobertLaas <laas.robert@gmail.com>
2024-12-03 13:48:24 +0100

    Ticket 1.4  vorerst fertig

--- MathMaster/MathMaster/Views/SetupView.swift
+++ MathMaster/MathMaster/Views/SetupView.swift
@@ -8,18 +8,44 @@
 import SwiftUI
 
 struct SetupView: View {
-
-    @AppStorage("selectedPicker") private var selectedPicker = Schwierigkeitsgrad.easy
+    //TODO: refektor
+    @AppStorage("Schwierigkeitsgrad") private var selectedPicker = Schwierigkeitsgrad.easy
+    @AppStorage("gameDuration") private var gameDuration = 60
+    @State private var showSheet = false
     
     var body: some View {
         
+        VStack {
+            Text("Schwierigkeitsgrad wählen")
+                .font(.headline)
+                .bold()
             Picker("Schwierigkeitsgrad", selection: $selectedPicker) {
                 ForEach(Schwierigkeitsgrad.allCases) { gameType in
                     Text(gameType.rawValue).tag(gameType)
                 }
             }
             .pickerStyle(.segmented)
-            .padding()
+            
+            Stepper("Rundenlänge: \(gameDuration) sek.", value: $gameDuration, in: (30...90), step: 10)
+            
+            Spacer()
+            
+            Button {
+                showSheet = true
+            } label: {
+                Text("Statistik")
+            }
+            .buttonStyle(.borderedProminent)
+            .sheet(isPresented: $showSheet) {
+                StatsView()
+                    .presentationDetents([.large,.medium])
+            }
+            
+            
+        }
+        .padding()
+        
+        
         
         
     }
