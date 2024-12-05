4005220bfce045854a5dc63222ff8d91ba38be46
christophschwalm <chris@christoph-schwalm.de>
2024-12-05 15:54:06 +0100

    Donnerstag Done

--- MathMaster/MathMaster/Util/MathHelper.swift
+++ MathMaster/MathMaster/Util/MathHelper.swift
@@ -9,70 +9,62 @@ import SwiftUI
 
 /// MathHelper zur Generierung von Rechenaufgaben
 struct MathHelper {
-
-    @AppStorage("additionToggle") private static var additionToggle = false
-    @AppStorage("subtractionToggle") private static var subtractionToggle = false
-    @AppStorage("multiplicationToggle") private static var multiplicationToggle = false
-    @AppStorage("divisionToggle") private static var divisionToggle = false
-
-
-
-
-    static func generateRandomMath() -> Math {
-        @AppStorage("difficultyLevel") var difficultyLevel: String = "Leicht"
-        
-        var maxResult: Int = 20
-        
-        if let level = Schwierigkeitsgrad.from(string: difficultyLevel) {
-            switch level {
-                case .easy : maxResult = 20
-                case .medium: maxResult = 50
-                case .hard: maxResult = 100
-            }
-        }
-        
-        var operations: [MathOperation] = []
-        //let operation = MathOperation.allCases.randomElement()!
-        if additionToggle {
-            operations.append(MathOperation.addition)
-        }
-        if subtractionToggle {
-            operations.append(MathOperation.subtraction)
-        }
-        if multiplicationToggle {
-            operations.append(MathOperation.multiplication)
-        }
-        if divisionToggle {
-            operations.append(MathOperation.division)
-        }
-        let operation = operations.randomElement() ?? MathOperation.addition
-        
-        print (Schwierigkeitsgrad.from(string: difficultyLevel))
-        
-        
-        
-        var number1: Int
-        var number2: Int
-        var result: Int
-
-        repeat {
-            number1 = Int.random(in: 1...maxResult)
-            number2 = Int.random(in: 1...maxResult)
-            print("\(number1) \(number2)")
-            switch operation {
-                case .addition: result = number1 + number2
-                case .subtraction: result = number1 - number2
-                case .multiplication: result = number1 * number2
-                case .division:
-                    if number2 != 0 {
-                        //result = number1
-                        result = number1 * number2
-                    } else {
-                        result = maxResult + 1
-                    }
-            }
-        } while result > maxResult || result < 0
-
-        return Math(displayText: "\(number1) \(operation.displayOperator) \(number2)", result: result)
+  @AppStorage("Schwierigkeitsgrad") private static var selectedDifficultyString: String = "Leicht"
+  @AppStorage("additionToggle") private static var additionToggle = false
+  @AppStorage("subtractionToggle") private static var subtractionToggle = false
+  @AppStorage("multiplicationToggle") private static var multiplicationToggle = false
+  @AppStorage("divisionToggle") private static var divisionToggle = false
+  static func generateRandomMath() -> Math {
+    var maxResult: Int = 20
+    var selectedDifficulty: Schwierigkeitsgrad {
+      get {
+        return Schwierigkeitsgrad(rawValue: selectedDifficultyString) ?? .easy
+      }
+      set {
+        selectedDifficultyString = newValue.rawValue
+      }
     }
+    switch selectedDifficulty {
+      case .easy : maxResult = 20
+      case .medium: maxResult = 50
+      case .hard: maxResult = 100
+    }
+    var operations: [MathOperation] = []
+    //let operation = MathOperation.allCases.randomElement()!
+    if additionToggle {
+      operations.append(MathOperation.addition)
+    }
+    if subtractionToggle {
+      operations.append(MathOperation.subtraction)
+    }
+    if multiplicationToggle {
+      operations.append(MathOperation.multiplication)
+    }
+    if divisionToggle {
+      operations.append(MathOperation.division)
+    }
+    let operation = operations.randomElement() ?? MathOperation.addition
+    print (selectedDifficulty)
+    var number1: Int
+    var number2: Int
+    var result: Int
+    repeat {
+      number1 = Int.random(in: 1...maxResult)
+      number2 = Int.random(in: 1...maxResult)
+      print("\(number1) \(number2)")
+      switch operation {
+        case .addition: result = number1 + number2
+        case .subtraction: result = number1 - number2
+        case .multiplication: result = number1 * number2
+        case .division:
+          if number2 != 0 {
+            //result = number1
+            result = number1 * number2
+          } else {
+            result = maxResult + 1
+          }
+      }
+    } while result > maxResult || result < 0
+    return Math(displayText: "\(number1) \(operation.displayOperator) \(number2)", result: result)
+  }
 }
--- MathMaster/MathMaster/Views/SetupView.swift
+++ MathMaster/MathMaster/Views/SetupView.swift
@@ -8,98 +8,80 @@
 import SwiftUI
 
 struct SetupView: View {
-    @AppStorage("user") private var userID: String = ""
-    
-    @AppStorage("Schwierigkeitsgrad") private var selectedPicker = "Robert"
-    @AppStorage("gameDuration") private var gameDuration = 60
-    @AppStorage("additionToggle") private var additionToggle = false
-    @AppStorage("subtractionToggle") private var subtractionToggle = false
-    @AppStorage("multiplicationToggle") private var multiplicationToggle = false
-    @AppStorage("divisionToggle") private var divisionToggle = false
-    
-    @State var currentTheme: Theme = .orange
-    
-    var body: some View {
-        
-        NavigationStack {
-            VStack {
-                Text("Schwierigkeitsgrad wählen")
-                    .font(.headline)
-                    .bold()
-                Picker("Schwierigkeitsgrad", selection: $selectedPicker) {
-                    ForEach(Schwierigkeitsgrad.allCases) { gameType in
-                        Text(gameType.rawValue).tag(gameType.rawValue)
-                    }
-                }
-                .pickerStyle(.segmented)
-                .padding()
-                
-                //TODO: hhhh
-                .onChange(of: selectedPicker) {
-                    print(Schwierigkeitsgrad.from(string: selectedPicker))
-                }
-                    Form {
-                    Section("Rundenlänge") {
-                        Stepper("Rundenlänge: \(gameDuration) sek.", value: $gameDuration, in: (10...90), step: 10)
-                        
-                        NavigationLink("Statistik"){
-                            StatsView(userID: userID)
-                        }
-                    }
-                    Section {
-                        HStack(spacing: 10) {
-                            Spacer()
-                            
-                            ForEach(Theme.allCases, id: \.rawValue) { theme in
-                                let size: CGFloat = 150
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(theme.bgGradient)
-                                    .opacity(currentTheme == theme ? 1 : 0.5)
-                                    .overlay {
-                                        if currentTheme == theme {
-                                            withAnimation {
-                                                RoundedRectangle(cornerRadius: 20)
-                                                    .stroke(.white.opacity(0.75), lineWidth: 5)
-                                            }
-                                        }
-                                    }
-                                    .frame(width: size, height: size)
-                                    .onTapGesture {
-                                        withAnimation {
-                                            currentTheme = theme
-                                        }
-                                    }
-                            }
-                            
-                            Spacer()
-                        }
-                        Picker("Theme", selection: $currentTheme) {
-                            ForEach(Theme.allCases, id: \.rawValue) { theme in
-                                Text(theme.rawValue).tag(theme)
-                            }
-                        }
-                        .pickerStyle(.segmented)
-                        .onChange(of: currentTheme, {
-                            ThemeService.setTheme(currentTheme)
-                        })
-                    }
-                    
-                    Section("Grundrechenarten") {
-                        Toggle("Addition", systemImage: "plus", isOn: $additionToggle)
-                        Toggle("Subtraktion", systemImage: "minus", isOn: $subtractionToggle)
-                        Toggle("Multiplikation", systemImage: "multiply", isOn: $multiplicationToggle)
-                        Toggle("Division", systemImage: "divide", isOn: $divisionToggle)
-                    }
-                }
+  @AppStorage("user") private var userID: String = ""
+  @AppStorage("Schwierigkeitsgrad") private var selectedDifficultyString: String = "Leicht"
+  @AppStorage("gameDuration") private var gameDuration = 60
+  @AppStorage("additionToggle") private var additionToggle = false
+  @AppStorage("subtractionToggle") private var subtractionToggle = false
+  @AppStorage("multiplicationToggle") private var multiplicationToggle = false
+  @AppStorage("divisionToggle") private var divisionToggle = false
+  @State var currentTheme: Theme = .orange
+  var body: some View {
+    NavigationStack {
+      VStack {
+        Text("Schwierigkeitsgrad wählen")
+          .font(.headline)
+          .bold()
+        Picker("Schwierigkeitsgrad", selection: $selectedDifficultyString) {
+          ForEach(Schwierigkeitsgrad.allCases) { gameType in
+            Text(gameType.rawValue).tag(gameType.rawValue)
+          }
+        }
+        .pickerStyle(.segmented)
+        .padding()
+        Form {
+          Section("Rundenlänge") {
+            Stepper("Rundenlänge: \(gameDuration) sek.", value: $gameDuration, in: (10...90), step: 10)
+            NavigationLink("Statistik"){
+              StatsView(userID: userID)
             }
+          }
+          Section {
+            HStack(spacing: 10) {
+              Spacer()
+              ForEach(Theme.allCases, id: \.rawValue) { theme in
+                let size: CGFloat = 150
+                RoundedRectangle(cornerRadius: 20)
+                  .fill(theme.bgGradient)
+                  .opacity(currentTheme == theme ? 1 : 0.5)
+                  .overlay {
+                    if currentTheme == theme {
+                      withAnimation {
+                        RoundedRectangle(cornerRadius: 20)
+                          .stroke(.white.opacity(0.75), lineWidth: 5)
+                      }
+                    }
+                  }
+                  .frame(width: size, height: size)
+                  .onTapGesture {
+                    withAnimation {
+                      currentTheme = theme
+                    }
+                  }
+              }
+              Spacer()
+            }
+            Picker("Theme", selection: $currentTheme) {
+              ForEach(Theme.allCases, id: \.rawValue) { theme in
+                Text(theme.rawValue).tag(theme)
+              }
+            }
+            .pickerStyle(.segmented)
+            .onChange(of: currentTheme, {
+              ThemeService.setTheme(currentTheme)
+            })
+          }
+          Section("Grundrechenarten") {
+            Toggle("Addition", systemImage: "plus", isOn: $additionToggle)
+            Toggle("Subtraktion", systemImage: "minus", isOn: $subtractionToggle)
+            Toggle("Multiplikation", systemImage: "multiply", isOn: $multiplicationToggle)
+            Toggle("Division", systemImage: "divide", isOn: $divisionToggle)
+          }
         }
-        .onAppear {
-            currentTheme = ThemeService.currentTheme
-        }
+      }
     }
-}
-
-
-#Preview {
-    SetupView()
+    .onAppear {
+      currentTheme = ThemeService.currentTheme
+    }
+  }
 }
