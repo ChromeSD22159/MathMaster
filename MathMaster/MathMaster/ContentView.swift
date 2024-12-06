3127171b91240063d06ecbc3f0a35175408300bb
Maria Schulte <maria_horn_dessau@hotmail.com>
2024-12-06 13:03:46 +0100

    content

--- MathMaster/MathMaster/ContentView.swift
+++ MathMaster/MathMaster/ContentView.swift
@@ -1,26 +1,21 @@
 //
-//  ContentView.swift
-//  MathMaster
+// ContentView.swift
+// MathMaster
 //
-//  Created by Leonid Riedel on 02.12.24.
+// Created by Leonid Riedel on 02.12.24.
 //
-
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    
     @Query var users: [User]
-    
     @AppStorage(AppStorageKey.user.rawValue) private var userID: String = ""
     @State var user: User? = nil
-    
     var body: some View {
         TabView {
             Tab("Game", systemImage: "gamecontroller") {
                 NavigationStack {
-                    HomeView(user: user) 
+                    HomeView(user: user)
                 }
             }
             Tab("Stats", systemImage: "list.clipboard.fill") {
@@ -36,32 +31,29 @@ struct ContentView: View {
         .onAppear {
             insertOrSetUser()
         }
-        onChange(of: userID) {
-              let user = users.first(where: { $0.userID == userID })
-              if let foundUser = user {
+        .onChange(of: userID) {
+            let user = users.first(where: { $0.userID == userID })
+            if let foundUser = user {
                 userID = foundUser.userID
                 self.user = foundUser
-              }
             }
+        }
     }
-    
     private func insertOrSetUser() {
         if users.count == 0 {
-          let user = User(name: "TestUser", age: 24)
-          modelContext.insert(user)
-          userID = user.userID
-          self.user = user
+            let user = User(name: "TestUser", age: 24)
+            modelContext.insert(user)
+            userID = user.userID
+            self.user = user
         } else {
-          let user = users.first(where: { $0.userID == userID })
-          if let foundUser = user {
-            userID = foundUser.userID
-            self.user = foundUser
-          }
+            let user = users.first(where: { $0.userID == userID })
+            if let foundUser = user {
+                userID = foundUser.userID
+                self.user = foundUser
+            }
         }
-      }
+    }
 }
-
-
 #Preview {
     ContentView()
         .modelContainer(previewContainer)
